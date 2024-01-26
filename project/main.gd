adb8540f9dd2c6f0d1c1755de7e3dde6b520c45e
PurpleHamster1 <alexbarbu100@yahoo.com>
2024-01-26 17:18:47 +0200

    fix

--- project/main.gd
+++ project/main.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	OS.set_window_maximized(true)
+	get_window().mode = Window.MODE_MAXIMIZED
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
